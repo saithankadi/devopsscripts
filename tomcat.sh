7f1bd2a6b383eb81288516e5e7b7eb61277477a6
saithankadi <saik74005@gmail.com>
2025-04-09 12:58:46 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,26 @@
-amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.39/bin/apache-tomcat-10.1.39.tar.gz
+#!/bin/bash
+
+# Install Java
+sudo amazon-linux-extras install java-openjdk11 -y
+
+# Download Tomcat
+wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.39/bin/apache-tomcat-10.1.39.tar.gz
+
+# Extract it
 tar -zxvf apache-tomcat-10.1.39.tar.gz
+
+# Update tomcat-users.xml
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.39/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.39/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.39/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.39/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-10.1.39/conf/tomcat-users.xml
+
+# Allow remote access to manager app
 sed -i '21d' apache-tomcat-10.1.39/webapps/manager/META-INF/context.xml
 sed -i '22d' apache-tomcat-10.1.39/webapps/manager/META-INF/context.xml
+
+# Start Tomcat
 sh apache-tomcat-10.1.39/bin/startup.sh
+
+echo "Tomcat 10.1.39 installed and started successfully!"
